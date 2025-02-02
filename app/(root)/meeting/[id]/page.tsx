18eecaa7afe263b981b05250ef0e3a21ab44dab2
Mayank Sharma <165517164+Sharmamayankkkk@users.noreply.github.com>
2025-02-03 01:12:24 +0530

    Update page.tsx

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -19,32 +19,27 @@ const MeetingPage = () => {
 
   if (!isLoaded || isCallLoading) return <Loader />;
 
-  if (!call) {
-    return (
-      <p className="text-center text-3xl font-bold text-white">
-        Call Not Found
-      </p>
-    );
-  }
+  if (!call) return (
+    <p className="text-center text-3xl font-bold text-white">
+      Call Not Found
+    </p>
+  );
 
-  // Check if the user is allowed to join the call
-  const notAllowed =
-    call.type === 'invited' &&
-    (!user || !call.state.members.find((m) => m.user.id === user.id));
+  // get more info about custom call type:  https://getstream.io/video/docs/react/guides/configuring-call-types/
+  const notAllowed = call.type === 'invited' && (!user || !call.state.members.find((m) => m.user.id === user.id));
 
-  if (notAllowed) {
-    return <Alert title="You are not allowed to join this meeting" />;
-  }
+  if (notAllowed) return <Alert title="You are not allowed to join this meeting" />;
 
   return (
     <main className="h-screen w-full">
       <StreamCall call={call}>
         <StreamTheme>
-          {!isSetupComplete ? (
-            <MeetingSetup setIsSetupComplete={setIsSetupComplete} />
-          ) : (
-            <MeetingRoom apiKey={call.id} userToken={user?.id || ''} userData={user ?? { id: '', name: '', image: '' }} />
-          )}
+
+        {!isSetupComplete ? (
+          <MeetingSetup setIsSetupComplete={setIsSetupComplete} />
+        ) : (
+          <MeetingRoom />
+        )}
         </StreamTheme>
       </StreamCall>
     </main>
