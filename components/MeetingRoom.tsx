b5e34e304f1cc4c48a4da45e6a469d9cbd52f38e
Mayank Sharma <165517164+Sharmamayankkkk@users.noreply.github.com>
2025-02-02 16:39:45 +0530

    Update MeetingRoom.tsx

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -20,7 +20,6 @@ import {
   DropdownMenuTrigger,
 } from './ui/dropdown-menu';
 import Loader from './Loader';
-import EndCallButton from './EndCallButton';
 import MuteButton from './MuteButton'; // Import the new MuteButton component
 import { cn } from '@/lib/utils';
 
@@ -28,7 +27,6 @@ type CallLayoutType = 'grid' | 'speaker-left' | 'speaker-right';
 
 const MeetingRoom = () => {
   const searchParams = useSearchParams();
-  const isPersonalRoom = !!searchParams.get('personal');
   const router = useRouter();
   const [layout, setLayout] = useState<CallLayoutType>('speaker-left');
   const [showParticipants, setShowParticipants] = useState(false);
