4792d4c1937fb0c06247c8fb03b8f8ab221e3de7
Mayank PradeepKumar Sharma <165517164+Sharmamayankkkk@users.noreply.github.com>
2025-05-22 12:39:18 +0530

    Fixed loader

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,16 +1,61 @@
-import Image from 'next/image';
+import React from "react";
 
-const Loader = () => {
-  return (
-    <div className="flex-center h-screen w-full">
-      <Image
-        src="/icons/loading-circle.svg"
-        alt="Loading..."
-        width={50}
-        height={50}
+const Loader: React.FC = () => (
+  <div
+    style={{
+      margin: 0,
+      background: "white",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh",
+      width: "100vw",
+    }}
+  >
+    <div className="loader-container" style={{
+      position: "relative",
+      width: 300,
+      overflow: "hidden",
+      maxWidth: "80vw",
+    }}>
+      <img
+        src="/icons/KCS-Logo.png"
+        alt="KCS Logo"
+        className="loader-img"
+        style={{
+          width: "100%",
+          height: "auto",
+          display: "block",
+        }}
       />
+      <div className="shine" />
+      <style jsx>{`
+        .shine {
+          position: absolute;
+          top: 0;
+          left: -100%;
+          width: 100%;
+          height: 100%;
+          background: linear-gradient(
+            120deg,
+            transparent 0%,
+            rgba(255, 255, 255, 0.5) 50%,
+            transparent 100%
+          );
+          animation: shineAnim 2s linear infinite;
+          z-index: 2;
+        }
+        @keyframes shineAnim {
+          0% {
+            left: -100%;
+          }
+          100% {
+            left: 100%;
+          }
+        }
+      `}</style>
     </div>
-  );
-};
+  </div>
+);
 
 export default Loader;
